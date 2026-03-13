5c68907682e68bfc5d2cff817fc9aeaf32e705b8
Navicks <knamiki@groove.ne.jp>
2026-03-13 13:00:00 +0900

    fix: Change version info on API

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -9,7 +9,7 @@ from app.models.user import User
 
 app = FastAPI(
     title="fa API",
-    version="0.1.3",
+    version="0.1.4",
     description="fa API endpoints",
     docs_url=None,
     redoc_url=None,
